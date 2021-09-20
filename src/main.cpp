1f998fa13d31e3d1ee215f8cf2812b3e29e8748c
Zaquan Byron <zaquan.byron@gmail.com>
2021-09-19 20:46:32 -0400

    non blocking bike read

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,21 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int nImpulsi = 0;
-float nMetriUnaPedalata = 5.3;
-int nMetriPercorsi;
-float nKmPercorsi;
+int nImpulse = 0;
+float nMetersPerCycle = 5.3;
+int nMetersTraveled;
+float nKmTraveled;
 int nSeconds;
-float nVelocitaMedia;
+float nAverageSpeed;
 float nCalories;
 
+//bounce
+int counter = 0;       // how many times we have seen new value
+int reading;           // the current value read from the input pin
+int current_state = LOW;    // the debounced input value
+long milTime = 0;         // the last time the output pin was sampled
+int debounce_count = 10; // number of millis/samples to consider before declaring a debounced input
+ bool lowTriggered = false;
 void initDisplay(){
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   delay(2000);
@@ -34,11 +41,11 @@ void updateDisplay(){
 
   display.setTextSize(2);
   display.setCursor(0,0);
-  display.print(String(nKmPercorsi) + "KMPS");
+  display.print(String(nKmTraveled) + "KM");
 
   display.setTextSize(1);
   display.setCursor(0, 30);
-  display.print(String(nMetriPercorsi) + "KM");
+  display.print(String(nMetersTraveled) + "M");
   
   display.setCursor(0, 40);
   display.print("Time: " + String(nSeconds) + "s");
@@ -50,6 +57,65 @@ void updateDisplay(){
   display.display(); 
 }
 
+void updateBikeData() {
+   //------------Elaborations--------------
+  nImpulse++;
+  nMetersTraveled = nImpulse * nMetersPerCycle;
+  nKmTraveled = nMetersTraveled/1000.0;
+  nSeconds = millis() / 1000;
+  nAverageSpeed = nKmTraveled * 3600 / nSeconds;
+  nCalories = nImpulse/10.0;
+
+}
+
+void bikeSerialOut() {
+  Serial.println("Ped: ");
+  Serial.println(nImpulse);
+  Serial.println("  Km: ");
+  Serial.println(nKmTraveled);
+ 
+  Serial.println("Time (secs): ");
+  Serial.println(nSeconds);
+
+  Serial.println("Speed AVG: ");
+  Serial.println(nAverageSpeed);
+  
+  Serial.println("Calories: ");
+  Serial.println(nCalories);
+}
+
+void readBikeInput() {
+  if(millis() != milTime)
+  {
+   
+    reading = digitalRead(bikePin);
+    if(reading == LOW)
+      lowTriggered = true;
+
+    if(reading == current_state && counter > 0)
+    {
+      counter--;
+    }
+    if(reading != current_state)
+    {
+       counter++; 
+    }
+    // If the Input has shown the same value for long enough let's switch it
+    if(counter >= debounce_count)
+    {
+      counter = 0;
+      if( reading == HIGH && lowTriggered){
+        updateBikeData();
+        updateDisplay();
+        bikeSerialOut();
+        lowTriggered = false;
+      }
+        
+    }
+    milTime = millis();
+  }
+}
+
 void setup() {
   Wire.begin();
   initDisplay();
@@ -58,34 +124,30 @@ void setup() {
   Serial.println("Starting...");
 }
 
+
 void loop() {
-  while(digitalRead(bikePin)==LOW)
-     delay(10);
-  while(digitalRead(bikePin)==HIGH)
-     delay(10);
+  // while(digitalRead(bikePin)==LOW)
+  //    delay(10);
+  // while(digitalRead(bikePin)==HIGH)
+  //    delay(10);
 
-   //------------Elaborations--------------
-  nImpulsi++;
-  nMetriPercorsi = nImpulsi * nMetriUnaPedalata;
-  nKmPercorsi = nMetriPercorsi/1000.0;
-  nSeconds = millis() / 1000;
-  nVelocitaMedia = nKmPercorsi * 3600 / nSeconds;
-  nCalories = nImpulsi/10.0;
+  // if(digitalRead(bikePin)==HIGH)
+  //  Serial.println("HIGH");
+  // else
+  //   Serial.println("LOW");
+  //  //------------Elaborations--------------
+  // nImpulse++;
+  // nMetersTraveled = nImpulse * nMetersPerCycle;
+  // nKmTraveled = nMetersTraveled/1000.0;
+  // nSeconds = millis() / 1000;
+  // nAverageSpeed = nKmTraveled * 3600 / nSeconds;
+  // nCalories = nImpulse/10.0;
+  // updateDisplay();
+  // bikeSerialOut();
 
-  //----------Screen-----------------
-  updateDisplay();
-  Serial.println("Ped: ");
-  Serial.println(nImpulsi);
-  Serial.println("  Km: ");
-  Serial.println(nKmPercorsi);
- 
-  Serial.println("Time (secs): ");
-  Serial.println(nSeconds);
-
-  Serial.println("Speed AVG: ");
-  Serial.println(nVelocitaMedia);
+  readBikeInput();
   
-  Serial.println("Calories: ");
-  Serial.println(nCalories);
-
+  //----------OUT-----------------
+  
+  
 }
