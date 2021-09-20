e534386d354d5132dda302d27a77e0b471976453
Zaquan Byron <zaquan.byron@gmail.com>
2021-09-19 21:40:02 -0400

    bike class

---
+++ src/bike.cpp
@@ -0,0 +1,80 @@
+#include "bike.h"
+#include <Arduino.h>
+
+int bikePin;
+int nImpulse = 0;
+float nMetersPerCycle = 5.3;
+int nMetersTraveled;
+float nKmTraveled;
+int nSeconds;
+float nAverageSpeed;
+float nCalories;
+
+//bounce
+int counter = 0;       
+int reading;           
+int current_state = LOW;
+long milTime = 0;
+int debounce_count = 10; 
+bool lowTriggered = false;
+
+Bike::Bike(int pin) {
+    bikePin = pin;
+    pinMode(pin,INPUT);
+}
+
+void Bike::updateBikeData() {
+  nImpulse++;
+  nMetersTraveled = nImpulse *  5.3;// nMetersPerCycle;
+  nKmTraveled = nMetersTraveled/1000.0;
+  nSeconds = millis() / 1000;
+  nAverageSpeed = nKmTraveled * 3600 / nSeconds;
+  nCalories = nImpulse/10.0;
+
+}
+
+void Bike::bikeSerialOut() {
+  Serial.println("Ped: ");
+  Serial.println(nImpulse);
+  Serial.println("  Km: ");
+  Serial.println(nKmTraveled);
+ 
+  Serial.println("Time (secs): ");
+  Serial.println(nSeconds);
+
+  Serial.println("Speed AVG: ");
+  Serial.println(nAverageSpeed);
+  
+  Serial.println("Calories: ");
+  Serial.println(nCalories);
+}
+
+void Bike::readBikeInput() {
+  if(millis() != milTime)
+  {
+   
+    reading = digitalRead(bikePin);
+    if(reading == LOW)
+      lowTriggered = true;
+
+    if(reading == current_state && counter > 0)
+    {
+      counter--;
+    }
+    if(reading != current_state)
+    {
+       counter++; 
+    }
+    if(counter >= debounce_count)
+    {
+      counter = 0;
+      if( reading == HIGH && lowTriggered){
+        updateBikeData();
+        bikeSerialOut();
+        lowTriggered = false;
+      }
+        
+    }
+    milTime = millis();
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,16 @@
 #include <Adafruit_I2CDevice.h> 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "bike.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define bikePin 17
+
+Bike bike(17);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int nImpulse = 0;
-float nMetersPerCycle = 5.3;
-int nMetersTraveled;
-float nKmTraveled;
-int nSeconds;
-float nAverageSpeed;
-float nCalories;
-
-//bounce
-int counter = 0;       // how many times we have seen new value
-int reading;           // the current value read from the input pin
-int current_state = LOW;    // the debounced input value
-long milTime = 0;         // the last time the output pin was sampled
-int debounce_count = 10; // number of millis/samples to consider before declaring a debounced input
- bool lowTriggered = false;
 void initDisplay(){
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   delay(2000);
@@ -41,113 +28,32 @@ void updateDisplay(){
 
   display.setTextSize(2);
   display.setCursor(0,0);
-  display.print(String(nKmTraveled) + "KM");
+  display.print(String(bike.nKmTraveled) + "KM");
 
   display.setTextSize(1);
   display.setCursor(0, 30);
-  display.print(String(nMetersTraveled) + "M");
+  display.print(String(bike.nMetersTraveled) + "M");
   
   display.setCursor(0, 40);
-  display.print("Time: " + String(nSeconds) + "s");
+  display.print("Time: " + String(bike.nSeconds) + "s");
 
 
   display.setCursor(0, 50);
-  display.print("Calories: " + String(nCalories));
+  display.print("Calories: " + String(bike.nCalories));
  
   display.display(); 
-}
-
-void updateBikeData() {
-   //------------Elaborations--------------
-  nImpulse++;
-  nMetersTraveled = nImpulse * nMetersPerCycle;
-  nKmTraveled = nMetersTraveled/1000.0;
-  nSeconds = millis() / 1000;
-  nAverageSpeed = nKmTraveled * 3600 / nSeconds;
-  nCalories = nImpulse/10.0;
-
-}
-
-void bikeSerialOut() {
-  Serial.println("Ped: ");
-  Serial.println(nImpulse);
-  Serial.println("  Km: ");
-  Serial.println(nKmTraveled);
- 
-  Serial.println("Time (secs): ");
-  Serial.println(nSeconds);
-
-  Serial.println("Speed AVG: ");
-  Serial.println(nAverageSpeed);
-  
-  Serial.println("Calories: ");
-  Serial.println(nCalories);
-}
-
-void readBikeInput() {
-  if(millis() != milTime)
-  {
-   
-    reading = digitalRead(bikePin);
-    if(reading == LOW)
-      lowTriggered = true;
-
-    if(reading == current_state && counter > 0)
-    {
-      counter--;
-    }
-    if(reading != current_state)
-    {
-       counter++; 
-    }
-    // If the Input has shown the same value for long enough let's switch it
-    if(counter >= debounce_count)
-    {
-      counter = 0;
-      if( reading == HIGH && lowTriggered){
-        updateBikeData();
-        updateDisplay();
-        bikeSerialOut();
-        lowTriggered = false;
-      }
-        
-    }
-    milTime = millis();
-  }
 }
 
 void setup() {
   Wire.begin();
   initDisplay();
-  pinMode(bikePin,INPUT);
   Serial.begin(9600);
   Serial.println("Starting...");
 }
 
-
 void loop() {
-  // while(digitalRead(bikePin)==LOW)
-  //    delay(10);
-  // while(digitalRead(bikePin)==HIGH)
-  //    delay(10);
 
-  // if(digitalRead(bikePin)==HIGH)
-  //  Serial.println("HIGH");
-  // else
-  //   Serial.println("LOW");
-  //  //------------Elaborations--------------
-  // nImpulse++;
-  // nMetersTraveled = nImpulse * nMetersPerCycle;
-  // nKmTraveled = nMetersTraveled/1000.0;
-  // nSeconds = millis() / 1000;
-  // nAverageSpeed = nKmTraveled * 3600 / nSeconds;
-  // nCalories = nImpulse/10.0;
-  // updateDisplay();
-  // bikeSerialOut();
-
-  readBikeInput();
-  
-  //----------OUT-----------------
-  
+  bike.readBikeInput();
+  updateDisplay();  
   
 }
